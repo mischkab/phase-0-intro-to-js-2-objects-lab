368149830f025c21a7c5c1bd2718375d68497d02
Mischka Booley <mischka@gearbox.com.au>
2022-05-27 22:11:18 +1000

    complete object lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+  name : 'Mischka',
+  streetAddress : 'Explorer Street'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, {[key]:value});
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+  return Object.assign(employee, {[key]:value});
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newObj =  Object.assign({}, employee);
+  delete newObj[key];
+  return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+};
